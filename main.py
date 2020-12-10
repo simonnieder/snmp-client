3a17bab359ad894be28c2b04e254da24d3fdda7a
unknown <niederwolfsgruber16@gmail.com>
2020-12-10 11:32:33 +0100

    commit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import snmpFunctions
 
 while True:
-    operation = input("What do you want to do? Type /get to perform get operation, /set to perform set operation, /get-auto to get basic snmp information, /listen to listen to traps\n")
+    operation = input("What do you want to do? Type /get to perform get operation, /set to perform set operation\n")
     if operation == "/get":
         ip = input('Enter the IP you want to use\n')
         community = input('Enter the community (default is "public") press enter to skip input and keep default)\n')
@@ -17,17 +17,8 @@ while True:
         oid = input('Enter the oid whose value you want to set\n')
         value = input('Enter value\n')
         snmpFunctions.set(ip, str(oid), value, community)
-    elif operation == "/get-auto":
-        ip = input('Enter the IP you want to use\n')
-        community = input('Enter the community (default is "private") press enter to skip and keep default)\n')
-        if not community:
-            community = "private"
-        oids = [".1.3.6.1.2.1.1.3.0", ".1.3.6.1.2.1.1.4.0", ".1.3.6.1.2.1.1.5.0", ".1.3.6.1.2.1.1.1.0", ".1.3.6.1.2.1.25.1.6.0"]
-        for oid in oids:
-            snmpFunctions.get(ip, oid, community)
     else:
         print("Command invalid. Try again")
-
 
 
 '''
--- test.py
+++
@@ -1,3 +0,0 @@
-import snmpFunctions
-
-snmpFunctions.get("localhost", ".1.3.6.1.2.1.1.5.0", "public")
