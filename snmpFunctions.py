041746092b96d0b613e078b85abb6c7e9eefaaa6
simonnieder <niederwolfsgruber16@gmail.com>
2020-12-11 18:22:48 +0100

    some improvements

--- snmpFunctions.py
+++ snmpFunctions.py
@@ -5,41 +5,69 @@ from pysnmp.entity import engine, config
 from pysnmp.carrier.asyncore.dgram import udp
 from pysnmp.entity.rfc3413 import ntfrcv
 
-def listen(): #listens for incoming traps  #! currently no way of stopping the function
-    Port=163 
-    myEngine = engine.SnmpEngine()
-    print("Listening on port:" + str(Port))
-    config.addTransport(
-        myEngine,
-        udp.domainName + (1,),
-        udp.UdpTransport().openServerMode(("localhost", Port))
-    )
-
-    #Configure community here
-    config.addV1System(myEngine, 'community', 'public')
-    def cbFun(snmpEngine, stateReference, contextEngineId, contextName,
-            varBinds, cbCtx):
-        print("Received new Trap message")
-        for name, val in varBinds:        
-            print(f"{name.prettyPrint()} = {val.prettyPrint()}")
-
-    ntfrcv.NotificationReceiver(myEngine, cbFun)
-
-    myEngine.transportDispatcher.jobStarted(1)  
-
+def listen(port): #listens for incoming traps  #! currently no way of stopping the function
     try:
-        myEngine.transportDispatcher.runDispatcher()
-    except:
-        myEngine.transportDispatcher.closeDispatcher()
-        raise
+        myEngine = engine.SnmpEngine()
+        print("Listening on port:" + str(port))
+        config.addTransport(
+            myEngine,
+            udp.domainName + (1,),
+            udp.UdpTransport().openServerMode(("localhost", port))
+        )
+
+
+        config.addV1System(myEngine, 'community', 'public')
+        def printFunc(snmpEngine, stateReference, contextEngineId, contextName,
+                varBinds, cbCtx):
+            print("Received new Trap message")
+            for name, val in varBinds:        
+                print(f"{name.prettyPrint()} = {val.prettyPrint()}")
+
+        ntfrcv.NotificationReceiver(myEngine, printFunc)
+
+        myEngine.transportDispatcher.jobStarted(1)  
+
+        try:
+            myEngine.transportDispatcher.runDispatcher()
+        except:
+            myEngine.transportDispatcher.closeDispatcher()
+            raise
+    except KeyboardInterrupt:
+        pass
     
-def get(ip, oid, communityName): #executes SNMP GET-operation
+def get(ip, communityName, oid, isThreaded = False): #executes SNMP GET-operation
     iterator = getCmd(
         SnmpEngine(),
         CommunityData(communityName),
         UdpTransportTarget((ip, 161)),
         ContextData(),
         ObjectType(ObjectIdentity(oid)))
+    
+    errorIndication, errorStatus, errorIndex, varBinds = next(iterator)
+    
+    if errorIndication:
+        if isThreaded == False:
+            print(f"{ip} not reachable")
+    elif errorStatus:
+        print('%s at %s' % (errorStatus.prettyPrint(), errorIndex and varBinds[int(errorIndex) - 1][0] or '?'))
+    else:
+        if isThreaded:
+            for varBind in varBinds:
+                print(f"{ip}: OID = {oid}, VALUE = {varBind[1]}")
+        else:
+            for varBind in varBinds:
+                return varBind[1]
+
+def getWithMib(ip, communityName, oid, filename): #executes SNMP GET-operation #! not working at all
+    iterator = getCmd(
+        SnmpEngine(),
+        CommunityData(communityName),
+        UdpTransportTarget((ip, 161)),
+        ContextData(),
+        ObjectType(ObjectIdentity(oid).addAsn1MibSource(
+            'file:///usr/share/snmp', 
+            filename
+        )))
     
     errorIndication, errorStatus, errorIndex, varBinds = next(iterator)
     
@@ -53,9 +81,9 @@ def get(ip, oid, communityName): #executes SNMP GET-operation
             for x in varBinds:
                 result = result +  " " + str(x)
         print(result)
-                
+   
     
-def set(ip, oid, value, communityName): #executes SNMP SET-operation
+def set(ip, communityName, oid, value): #executes SNMP SET-operation
     iterator = setCmd(
         SnmpEngine(),
         CommunityData(communityName),
@@ -69,13 +97,19 @@ def set(ip, oid, value, communityName): #executes SNMP SET-operation
     elif errorStatus:
         print('%s at %s' % (errorStatus.prettyPrint(), errorIndex and varBinds[int(errorIndex) - 1][0] or '?'))
     else:
-        print('Value set\n')
+        print(f'Value({value}) has been set!\n')
             
             
 def scanNetwork(network, community): #iterates through all ips of a given network and makes a snmp request to get host name
+    threads = []
+    counter = 0
     for ip in ipaddress.IPv4Network(network):
         oids = [".1.3.6.1.2.1.1.5.0"]
         for oid in oids:
-            thread = Thread(target = get,args =(str(ip), oid, community))
+            thread = Thread(target = get,args =(str(ip), community, oid, True))
             thread.start()
+            threads.append(thread)
+            counter+=1
 
+    for thread in threads:
+        thread.join()
